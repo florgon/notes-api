7df5cd01940a2f7696744c11118cf494baa2cdab
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-24 11:37:53 +0400

    Fix exception handlers

--- src/notes-api/app/exception_handlers/__init__.py
+++ src/notes-api/app/exception_handlers/__init__.py
@@ -16,7 +16,7 @@ def register_handlers(app: FastAPI) -> None:
         Adds exception handlers for application.
     """
 
-    app.exception_handler(ApiErrorException, _handlers._api_error_exception_handler)
-    app.exception_handler(RequestValidationError, _handlers._validation_exception_handler)
-    app.exception_handler(404, _handlers._not_found_handler)
-    app.exception_handler(500, _handlers._internal_server_error_handler)
+    app.add_exception_handler(ApiErrorException, _handlers._api_error_exception_handler)
+    app.add_exception_handler(RequestValidationError, _handlers._validation_exception_handler)
+    app.add_exception_handler(404, _handlers._not_found_handler)
+    app.add_exception_handler(500, _handlers._internal_server_error_handler)
